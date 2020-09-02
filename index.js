4e0a35627ff20694f98a636ca78ac53513f80900
Johnson Liu <jyliu17@gmail.com>
2020-09-02 01:35:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
+var ticketNumber = 1;
+
+function takeANumber(line) {
+ 
+  
+  line.push(ticketNumber++);
+ 
+    return `Welcome, You are number ${ticketNumber} in line.`
+
+}
+
+
+
+
 function takeANumber(line, name) {
   line.push(name);
 
@@ -14,14 +28,15 @@ function nowServing(katzDeliLine) {
 }
 
 function currentLine(katzDeliLine) {
+
   var inline = [];
   if (katzDeliLine.length === 0) {
     return "The line is currently empty."
   } else {
       for(var i = 0; i < katzDeliLine.length; i++ ){
         inline.push(`${i+1}. ${katzDeliLine[i]}`);
-      }
-      inline = inline.join( ", "+ "");
+      } inline = inline.join( ","+ " ");
+     
       return `The line is currently: ` + inline;
 }
 }
